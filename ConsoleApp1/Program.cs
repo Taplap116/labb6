b79a04451a2a9a1bada298d13805965ac9ac1019
danil <dddd@gmail.com>
2024-02-01 01:19:01 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿double f(double nn)
+{
+    double x = 1;
+    for (int i = 1; i <= nn; i++)
+        x = x * i;
+    return x;
+}
+double s(double a,double k)
+{
+    double x = 1;
+    for (int i = 1; i <= k; i++)
+        x = x * a;
+    return x;
+}
+Console.WriteLine("введите x,c,k");
+double x=  double.Parse(Console.ReadLine());
+double c = double.Parse(Console.ReadLine());
+double k = double.Parse(Console.ReadLine());
+double y = (s(c - x, k) / (s(c, k) * (f(c) - f(k))));
+Console.WriteLine($"y={y}");
---
+++ labb6/Program.cs
@@ -0,0 +1,26 @@
+﻿void factorial(double nn, out double x)
+{
+    x = 1;
+    for (int i = 1; i < +nn; i++)
+    {
+        x = x * i;
+    }
+}
+void stepen(double a, double k, out double x)
+{
+    x = 1;
+    for (int i = 1; i <= k; i++)
+    {
+        x = x * a;
+    }
+}
+Console.WriteLine("введите x,c,k");
+double x = double.Parse(Console.ReadLine());
+double c = double.Parse(Console.ReadLine());
+double k = double.Parse(Console.ReadLine());
+stepen(c - x, k, out double s1);
+stepen(c, k, out double s2);
+factorial(c, out double p1);
+factorial(k, out double p2);
+double f = s1 / (s2 * (p1 - p2));
+Console.WriteLine($"f = {f}");
